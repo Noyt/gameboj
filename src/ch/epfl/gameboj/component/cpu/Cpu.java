c28062ad04ed98c76c227b7f2737fb39a5c58d11
Arnaud Robert <arnaudemmanuel.robert@epfl.ch>
2018-03-06 20:18:03 +0100

    tout sauf cylce dans Cpu

--- src/ch/epfl/gameboj/component/cpu/Cpu.java
+++ src/ch/epfl/gameboj/component/cpu/Cpu.java
@@ -1,21 +1,21 @@
 package ch.epfl.gameboj.component.cpu;
 
-import java.util.ArrayList;
-
 import ch.epfl.gameboj.Bus;
+import ch.epfl.gameboj.Preconditions;
 import ch.epfl.gameboj.Register;
 import ch.epfl.gameboj.RegisterFile;
+import ch.epfl.gameboj.bits.Bits;
 import ch.epfl.gameboj.component.Clocked;
 import ch.epfl.gameboj.component.Component;
-import ch.epfl.gameboj.component.cpu.Cpu.Reg;
 
 public final class Cpu implements Component, Clocked {
 
     private RegisterFile<Reg> file;
+    // TODO juste de déclarer SP et PC en int ?
     private int SP;
     private int PC;
-    private Bus bus; //TODO arraylist ou bus tout seul?
-    
+    private Bus bus; // TODO arraylist ou bus tout seul?
+
     private long nextNonIdleCycle;
 
     private enum Reg implements Register {
@@ -26,14 +26,13 @@ public final class Cpu implements Component, Clocked {
         AF, BC, DE, HL
     }
 
-    
     public Cpu() {
         file = new RegisterFile<Reg>(Reg.values());
         SP = 0;
         PC = 0;
         nextNonIdleCycle = 0;
     }
-    
+
     @Override
     public void cycle(long cycle) {
         if (cycle < nextNonIdleCycle) {
@@ -55,7 +54,7 @@ public final class Cpu implements Component, Clocked {
 
     }
 
-    //TODO quel visibilité?
+    // TODO quel visibilité?
     protected void dispatch(Opcode instruction) {
         switch (instruction.family) {
         case NOP: {
@@ -129,41 +128,168 @@ public final class Cpu implements Component, Clocked {
             break;
         }
     }
-    
+
     /*
-     * ------------------------------      Bus methods      -----------------------------
+     * ------------------------------ Bus methods -----------------------------
      */
     public void attachTo(Bus bus) {
         Component.super.attachTo(bus);
         this.bus = bus;
     }
-    
-    private int read8(int address);
-    private int read8AtHl();
-    int read8AfterOpcode();
-    int read16(int address);
-    int read16AfterOpcode();
-    void write8(int address, int v);
-    void write16(int address, int v);
-    void write8AtHl(int v);
-    void push16(int v);
-    int pop16();
-    
+
+    // TODO preconditions pour un résultat uniquement 8 bits
+    private int read8(int address) {
+        Preconditions.checkBits16(address);
+        int value = bus.read(address);
+        Preconditions.checkBits8(value);
+        return value;
+    };
+
+    private int read8AtHl() {
+        return read8(reg16(Reg16.HL));
+    };
+
+    int read8AfterOpcode() {
+        return read8(PC + 1);
+    };
+
+    int read16(int address) {
+        Preconditions.checkBits16(address);
+
+        // int value = read8(address);
+        // int low = Bits.extract(value, Byte.SIZE, Byte.SIZE);
+        // int high = Bits.clip(value, Byte.SIZE);
+
+        int low = read8(address);
+        int high = read8(address + 1);
+
+        return Bits.make16(high, low);
+    };
+
+    int read16AfterOpcode() {
+        Preconditions.checkBits16(PC + 1);
+        return read16(PC + 1);
+    };
+
+    void write8(int address, int v) {
+        Preconditions.checkBits16(address);
+        Preconditions.checkBits8(v);
+        bus.write(address, v);
+    };
+
+    void write16(int address, int v) {
+        Preconditions.checkBits16(v);
+        Preconditions.checkBits16(v);
+        int high = Bits.extract(v, Byte.SIZE, Byte.SIZE);
+        int low = Bits.clip(v, Byte.SIZE);
+        bus.write(address, low);
+        bus.write(address + 1, high);
+    };
+
+    void write8AtHl(int v) {
+        Preconditions.checkBits8(v);
+        write8(reg16(Reg16.HL), v);
+    };
+
+    void push16(int v) {
+        Preconditions.checkBits16(v);
+        SP -= 2;
+        if (SP < 0) {
+            SP = 0;
+        }
+        write16(SP, v);
+    };
+
+    int pop16() {
+        SP += 2;
+        return read16(SP - 2);
+    };
+
     /*
-     * ---------------------      Extract information from Opcodes      ------------------
+     * --------------------- Extract information from Opcodes ------------------
      */
-    private Reg extractReg(Opcode opcode, int startBit);
-    private Reg16 extractReg(Opcode opcode);
-    private int extractHlIncrement(Opcode opcode);
-    
-    
+    private Reg extractReg(Opcode opcode, int startBit) {
+        int reg = Bits.extract(opcode.encoding, startBit, 3);
+        switch (reg) {
+        case 0b000:
+            return Reg.B;
+        case 0b001:
+            return Reg.C;
+        case 0b010:
+            return Reg.D;
+        case 0b011:
+            return Reg.E;
+        case 0b100:
+            return Reg.H;
+        case 0b101:
+            return Reg.L;
+        case 0b111:
+            return Reg.A;
+        default:
+            throw new IllegalArgumentException("ceci est faux, 110 registre");
+        }
+    };
+
+    private Reg16 extractReg16(Opcode opcode) {
+        int reg = Bits.extract(opcode.encoding, 4, 2);
+        switch (reg) {
+        case 0b00:
+            return Reg16.BC;
+        case 0b01:
+            return Reg16.DE;
+        case 0b10:
+            return Reg16.HL;
+        case 0b11:
+            return Reg16.AF;
+        default:
+            throw new IllegalArgumentException("TODO");
+        }
+    }
+
+    private int extractHlIncrement(Opcode opcode) {
+        boolean reg = Bits.test(opcode.encoding, 4);
+        if (reg) {
+            return -1;
+        } else {
+            return 1;
+        }
+    };
+
     /*
-     * --------------------------      Registers Managements      -------------------------
+     * -------------------------- Registers Managements
+     * -------------------------
      */
-    private int reg16(Reg16 r);
-    private void setReg16(Reg16 r, int newV);
-    private void setReg16SP(Reg16 r, int newV);
-    
+    private int reg16(Reg16 r) {
+        Reg r1 = Reg.values()[r.index() * 2];
+        Reg r2 = Reg.values()[r.index() * 2 + 1];
+
+        return Bits.make16(file.get(r1), file.get(r2));
+    }
+
+    // TODO preconditions sur newV (16/8?)
+    private void setReg16(Reg16 r, int newV) {
+        Reg r1 = Reg.values()[r.index() * 2];
+        Reg r2 = Reg.values()[r.index() * 2 + 1];
+
+        int highV = Bits.extract(newV, Byte.SIZE, Byte.SIZE);
+        int lowV = Bits.clip(Byte.SIZE, newV);
+
+        file.set(r1, highV);
+        file.set(r2, lowV);
+
+        // TODO pour les 4 premiers zeros en cas AF, mieux vaut utiliser des
+        // méthodes de BIts comme set ou des << >>> ?
+    }
+
+    private void setReg16SP(Reg16 r, int newV) {
+        if (r == Reg16.AF) {
+            SP = newV;
+        } else {
+            setReg16(r, newV);
+        }
+
+    }
+
     // TODO
     public int[] _testGetPcSpAFBCDEHL() {
         return new int[1];
