f86bb9babe4bc95215a4b298716ee586cb43515f
Arnaud Robert <arnaudemmanuel.robert@epfl.ch>
2018-03-07 15:33:20 +0100

    petis trucs

--- src/ch/epfl/gameboj/component/cpu/Cpu.java
+++ src/ch/epfl/gameboj/component/cpu/Cpu.java
@@ -250,9 +250,8 @@ public final class Cpu implements Component, Clocked {
         boolean reg = Bits.test(opcode.encoding, 4);
         if (reg) {
             return -1;
-        } else {
-            return 1;
         }
+        return 1;
     };
 
     /*
