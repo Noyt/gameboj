a411a968808eeb36dc0daa0dfb4a5ca48bad2256
Arnaud Robert <arnaudemmanuel.robert@epfl.ch>
2018-04-09 11:48:39 +0200

    doc

--- src/ch/epfl/gameboj/Register.java
+++ src/ch/epfl/gameboj/Register.java
@@ -1,9 +1,31 @@
 package ch.epfl.gameboj;
 
+
+//TODO Sciper
+/**
+ * Register
+ * 
+ * @author Arnaud Robert
+ * @author Sophie Du Couedic
+ *
+ */
 public interface Register {
 
+    /**
+     * Is given by the enumeration representing the registers
+     * 
+     * @see java.lang.Enum<E>;
+     * @return the ordinal of this enumeration constant (its position in its
+     *         enum declaration, where the initial constant is assigned an
+     *         ordinal of zero)
+     */
     int ordinal();
-    
+
+    /**
+     * Returns the same value as the method ordinal()
+     * 
+     * @return
+     */
     public default int index() {
         return ordinal();
     }
