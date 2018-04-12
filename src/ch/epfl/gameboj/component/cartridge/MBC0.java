76bccb255695197e500526cbe00186ccbb1a7c61
so7fie <sophie.ducouedicdekergoualer@epfl.ch>
2018-04-12 21:27:10 +0200

    h

--- src/ch/epfl/gameboj/component/cartridge/MBC0.java
+++ src/ch/epfl/gameboj/component/cartridge/MBC0.java
@@ -6,6 +6,13 @@ import ch.epfl.gameboj.Preconditions;
 import ch.epfl.gameboj.component.Component;
 import ch.epfl.gameboj.component.memory.Rom;
 
+/**
+ * Represents a boot memory Controller of type 0 (can contain only a read-only
+ * memory of 32768 octets)
+ * 
+ * @author sophie
+ *
+ */
 public final class MBC0 implements Component {
 
     private Rom rom = null;
@@ -15,11 +22,12 @@ public final class MBC0 implements Component {
         Preconditions.checkArgument(rom.size() == 32768);
         this.rom = rom;
     }
-    
+
     @Override
     public int read(int address) {
         Preconditions.checkBits16(address);
-        // TODO la rom part elle vraiment de 0 (et au niveau de la rom de demarrage ?)
+        // TODO la rom part elle vraiment de 0 (et au niveau de la rom de
+        // demarrage ?)
         if (address > 0x7FFF) {
             return NO_DATA;
         } else {
@@ -29,9 +37,7 @@ public final class MBC0 implements Component {
 
     @Override
     public void write(int address, int data) {
-        
-    }
 
-    
+    }
 
 }
