d12a7494b38369145e878cbe1bd287efed761dd6
Arnaud Robert <arnaudemmanuel.robert@epfl.ch>
2018-04-15 16:48:22 +0200

    BitVector builder and other stuff

--- src/ch/epfl/gameboj/bits/BitVector.java
+++ src/ch/epfl/gameboj/bits/BitVector.java
@@ -2,19 +2,31 @@ package ch.epfl.gameboj.bits;
 
 import java.util.Objects;
 
+import javax.swing.text.ChangedCharSetException;
+
 import ch.epfl.gameboj.Preconditions;
 
 public final class BitVector {
 
+    public static final int ALL_ZEROS_INTEGER = 0b00000000_00000000_00000000_00000000;
+    public static final int ALL_ONES_INTEGER = 0b11111111_11111111_11111111_11111111;
+
     // TODO à discuter
     private final int[] vector;
 
+    // TODO essayer d'utiliser le BitVector privé dans ce constructeur ?
     public BitVector(int size, boolean initialValue) {
         Preconditions.checkArgument(size >= 0 && is32Multiple(size));
+
+        int numberOfInts = size / Integer.SIZE;
+        vector = new int[numberOfInts];
+        for (int i = 0; i < numberOfInts; i++) {
+            vector[i] = initialValue ? ALL_ONES_INTEGER : ALL_ZEROS_INTEGER;
+        }
     }
 
     public BitVector(int size) {
-        BitVector(size, false);
+        this(size, false);
     }
 
     // stocker sans copier
@@ -24,47 +36,55 @@ public final class BitVector {
 
     private static final class Builder {
 
-        //TODO à discuter
-        
-        private byte[] components;
-        
+        // TODO à discuter
+
+        private byte[] bytes;
+
         public Builder(int size) {
             Preconditions.checkArgument(size >= 0 && is32Multiple(size));
-            components = new byte[size / Byte.SIZE];
+            bytes = new byte[size / Byte.SIZE];
         }
 
         public Builder setByte(int index, int value) {
-            Objects.checkIndex(index, components.length);
+            checkIfBuiltAlready();
+            Objects.checkIndex(index, bytes.length);
             Preconditions.checkBits8(value);
-            components[index] = (byte) value;
+            bytes[index] = (byte) value;
             return this;
         }
 
         public BitVector build() {
-            int length = components.length;
-            int[] temp = new int[length / 4];
-            
-            int a;
-            
+            checkIfBuiltAlready();
+            int length = bytes.length;
+            int[] temp = new int[length / (Integer.SIZE / Byte.SIZE)];
+
             for (int i = 0; i < temp.length; i++) {
-                a = 0;
-                for (int j = 0; j < 4; j++) {
-                  a += Byte.toUnsignedInt(components[i + j]) << Byte.SIZE * j;  
+                int a = 0;
+                for (int j = 0; j < Integer.SIZE / Byte.SIZE; j++) {
+                    a += Byte.toUnsignedInt(bytes[i + j]) << Byte.SIZE * j;
                 }
                 temp[i] = a;
             }
-            
+
+            bytes = null;
             return new BitVector(temp);
+        }
+
+        // TODO nom peu compréhensible maybe
+        private void checkIfBuiltAlready() {
+            if (bytes == null) {
+                throw new IllegalStateException();
+            }
         }
     }
 
     public int size() {
-
+        return vector.length * Integer.SIZE;
     }
 
-
     public boolean testBit(int index) {
-
+        Objects.checkIndex(index, this.size());
+        return Bits.test(vector[index / Integer.SIZE], index % Integer.SIZE);
     }
 
     public BitVector not() {
@@ -99,15 +119,15 @@ public final class BitVector {
     public boolean equals(Object that) {
 
     }
-    
+
     @Override
     public int hashCode() {
-        
+
     }
-    
+
     @Override
     public String toString() {
-        
+
     }
 
     private static boolean is32Multiple(int a) {
