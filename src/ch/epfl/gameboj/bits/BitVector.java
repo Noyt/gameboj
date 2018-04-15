37cafc619026eb1e20aa31d2cece1f9ca9c757c0
Arnaud Robert <arnaudemmanuel.robert@epfl.ch>
2018-04-15 15:01:50 +0200

    BitVector structure

--- src/ch/epfl/gameboj/bits/BitVector.java
+++ src/ch/epfl/gameboj/bits/BitVector.java
@@ -4,6 +4,9 @@ import ch.epfl.gameboj.Preconditions;
 
 public final class BitVector {
 
+    // TODO à discuter
+    private final int[] vector;
+
     public BitVector(int size, boolean initialValue) {
         Preconditions.checkArgument(size >= 0 && is32Multiple(size));
     }
@@ -12,18 +15,23 @@ public final class BitVector {
         BitVector(size, false);
     }
 
+    // stocker sans copier
+    private BitVector(int[] elements) {
+
+    }
+
     public static final class Builder {
 
         public Builder(int size) {
             Preconditions.checkArgument(size >= 0 && is32Multiple(size));
         }
-        
+
         public Builder setByte(int index, int value) {
             Preconditions.checkBits8(value);
         }
-        
+
         public BitVector build() {
-            
+
         }
     }
 
@@ -61,6 +69,21 @@ public final class BitVector {
 
     private BitVector extract(int index, int length, boolean type) {
 
+    }
+
+    @Override
+    public boolean equals(Object that) {
+
+    }
+    
+    @Override
+    public int hashCode() {
+        
+    }
+    
+    @Override
+    public String toString() {
+        
     }
 
     private static boolean is32Multiple(int a) {
