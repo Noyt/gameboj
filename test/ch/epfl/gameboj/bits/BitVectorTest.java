438bc35eeb5afd0a7f5fa69f5d4db51f38069267
so7fie <sophie.ducouedicdekergoualer@epfl.ch>
2018-04-20 13:39:30 +0200

    h

--- test/ch/epfl/gameboj/bits/BitVectorTest.java
+++ test/ch/epfl/gameboj/bits/BitVectorTest.java
@@ -10,7 +10,6 @@ import ch.epfl.gameboj.bits.BitVector.Extraction;
 
 class BitVectorTest {
 
-
     private void fillBuilder(int[] array, Builder b) {
         for (int i = 0; i < array.length; ++i) {
             for (int j = 0; j < 4; ++j) {
@@ -19,7 +18,7 @@ class BitVectorTest {
             }
         }
     }
-    
+
     @Test
     void extractAndBuilderWorksForValidValue() {
         int[] array = { -1, 0 };
@@ -39,12 +38,7 @@ class BitVectorTest {
 
         int[] array2 = { -1, 0, 1431655765 };
         Builder testBuilder2 = new Builder(96);
-        for (int i = 0; i < array2.length; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                testBuilder2.setByte(i * 4 + j,
-                        Bits.extract(array2[i], j * Byte.SIZE, Byte.SIZE));
-            }
-        }
+        fillBuilder(array2, testBuilder2);
 
         BitVector test2 = testBuilder2.build();
         assertEquals(0b10101010_10101010_10101000_00000000,
@@ -62,5 +56,29 @@ class BitVectorTest {
         assertEquals(0, test2.extractP(-64, 128, Extraction.ZERO_EXTENDED)[0]);
         assertEquals(0, test2.extractP(-64, 128, Extraction.ZERO_EXTENDED)[1]);
         assertEquals(-1, test2.extractP(-64, 128, Extraction.ZERO_EXTENDED)[2]);
+    }
+
+    @Test
+    void toStringWorksForValidValues() {
+        int[] a = { 0b11110000_10101010_00000000_11001100 };
+        Builder b = new Builder(32);
+        fillBuilder(a, b);
+        BitVector test = b.build();
+        assertEquals("11110000101010100000000011001100", test.toString());
+
+        int[] a2 = { 0b11111111_00000000_11111111_00000000,
+                0b11110000_01010101_11110000_11111111 };
+        Builder b2 = new Builder(64);
+        fillBuilder(a2, b2);
+        BitVector test2 = b2.build();
+        assertEquals(
+                "1111111100000000111111110000000011110000010101011111000011111111",
+                test2.toString());
+        
+        int[] a3 = {};
+        Builder b3 = new Builder(0);
+        fillBuilder(a3, b3);
+        BitVector test3 = b3.build();
+        assertEquals("", test3.toString());
     }
 }
