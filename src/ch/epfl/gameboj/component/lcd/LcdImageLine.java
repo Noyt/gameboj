527a09e0f5c646dc9715ed98dea298f9e0190b41
Arnaud Robert <arnaudemmanuel.robert@epfl.ch>
2018-04-21 12:17:12 +0200

    map color in progress

--- src/ch/epfl/gameboj/component/lcd/LcdImageLine.java
+++ src/ch/epfl/gameboj/component/lcd/LcdImageLine.java
@@ -9,7 +9,7 @@ public final class LcdImageLine {
     private BitVector msb;
     private BitVector lsb;
     private BitVector opacity;
-
+    
     public LcdImageLine(BitVector msb, BitVector lsb, BitVector opacity) {
         Preconditions.checkArgument(
                 msb.size() == lsb.size() && msb.size() == opacity.size());
@@ -44,32 +44,45 @@ public final class LcdImageLine {
                 opacity.shift(pixels));
     }
 
+    // TODO check length multiple de 32
     public LcdImageLine extractWrapped(int pixel, int length) {
         return new LcdImageLine(msb.extractWrapped(pixel, length),
                 lsb.extractWrapped(pixel, length),
                 opacity.extractWrapped(pixel, length));
     }
 
+    //TODO ou preconditions check8
     public LcdImageLine mapColors(byte palette) {
-        //TODO checker palette
-        BitVector.Builder b = new BitVector.Builder(size());
-        
-        for (int i = 0; i < size() / Byte.SIZE; i++) {
-            
-            byte newColor = 0;
-            
-            for (int j = 0; j < Byte.SIZE; j++) {
-                
-                int substitustionColor = Bits.extract(palette,
-                        valueAtIndex((i*Byte.SIZE + j) * 2), 2);
-                
-                int msbValue = substitustionColor/2;
-                int lsbValue = substitustionColor%2;
-            }
+        if (palette == 0b11_10_01_00) {
+            return this;
         }
+
+        if(palette)
     }
 
+    // TODO a effacer potentiellement
     private int valueAtIndex(int index) {
         return (msb.testBit(index) ? 1 : 0) * 2 + (lsb.testBit(index) ? 1 : 0);
+    }
+
+    private BitVector identifyBitsOfColor(int color) {
+        Preconditions.checkArgument(color < 4 || color >=0);
+        switch (color) {
+        case 0b00:
+            return msb.or(lsb).not();
+        case 0b01:
+            return lsb.and(msb.not());
+        case 0b10:
+            return msb.and(lsb.not());
+        case 0b11:
+            return msb.and(lsb);
+        default:
+            throw new Error("incorret color");
+        }
+        
+    }
+    
+    private BitVector operateBitChange(BitVector vec, BitVector change) {
+        
     }
 }
