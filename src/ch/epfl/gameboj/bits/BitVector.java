af08c98f024776611a6c0bb4fdd27fddb6b588bd
so7fie <sophie.ducouedicdekergoualer@epfl.ch>
2018-04-25 21:47:12 +0200

    fini javadoc part 7

--- src/ch/epfl/gameboj/bits/BitVector.java
+++ src/ch/epfl/gameboj/bits/BitVector.java
@@ -8,6 +8,12 @@ import javax.swing.text.ChangedCharSetException;
 
 import ch.epfl.gameboj.Preconditions;
 
+/**
+ * A set of tools for bit vectors manipulations
+ * 
+ * @author Sophie du Couédic (260007)
+ * @author Arnaud Robert (287964)
+ */
 public final class BitVector {
 
     public static final int ALL_ZEROS_INTEGER = 0b00000000_00000000_00000000_00000000;
@@ -17,11 +23,23 @@ public final class BitVector {
     private final int[] vector;
 
     // TODO il faudra mettre en private quand on aura fini les tests
-    public enum Extraction {
+    private enum Extraction {
         WRAPPED, ZERO_EXTENDED
     };
 
-    // TODO essayer d'utiliser le BitVector privé dans ce constructeur ?
+    /**
+     * Constructs and returns a new BitVector, filled with only zeros or only
+     * ones
+     * 
+     * @param size
+     *            : an integer, the size (in bits) of the vector
+     * @param initialValue
+     *            : a boolean the value with which we will fill the vector
+     *            (false for zero and true for one)
+     * @throws IllegalArgumentException
+     *             : if the size is not strictly positive or if it is not a
+     *             multiple of 32
+     */
     public BitVector(int size, boolean initialValue) {
         Preconditions.checkArgument(size > 0 && is32Multiple(size));
 
@@ -32,32 +50,73 @@ public final class BitVector {
         }
     }
 
+    /**
+     * Constructs and returns a new BitVector, containing only zeros
+     * 
+     * @param size
+     *            : an integer : the size (in bits) of the vector
+     * 
+     * @throws IllegalArgumentException
+     *             : if the size is not strictly positive or if it is not a
+     *             multiple of 32
+     */
     public BitVector(int size) {
         this(size, false);
     }
 
-    // stocker sans copier
+    // for internal use only
     private BitVector(int[] elements) {
         vector = elements;
     }
 
+    /**
+     * Helps build a BitVector from scratch
+     * 
+     * @author Sophie du Couédic (260007)
+     * @author Arnaud Robert (287964)
+     */
     public static final class Builder {
 
         private boolean enable;
 
         private int[] bits;
 
+        /**
+         * Create and return a new Builder able to build a BitVector of the
+         * given size
+         * 
+         * @param size
+         *            : an integer the size of the future BitVector
+         */
         public Builder(int size) {
             Preconditions.checkArgument(size > 0 && is32Multiple(size));
-            bits = new int[size/Integer.SIZE];
+            bits = new int[size / Integer.SIZE];
             enable = true;
         }
 
+        /**
+         * defines the value of the given index's byte
+         * 
+         * @param index
+         *            : an integer, the index of the byte that we want to set
+         * @param value
+         *            : an integer, the 8-bits value we want to place in the
+         *            vector at the given index
+         * @return Builder : the builder itself for further settings
+         * @throws IllegalStateException
+         *             : if the builder has been already used to build some
+         *             BitVector
+         * @throws IndexOutOfBoundsException
+         *             : if the index is less than zero or if it is greater than
+         *             the size of the future BitVector, divided by 8
+         * @throws IllegalArgumentException
+         *             : if the value is not an 8-bits value
+         */
         public Builder setByte(int index, int value) {
             checkIfBuiltAlready();
-            
-            int ratioIntByte = Integer.SIZE/Byte.SIZE;
-            
+
+            int ratioIntByte = Integer.SIZE / Byte.SIZE;
+
             Objects.checkIndex(index, bits.length * ratioIntByte);
             Preconditions.checkBits8(value);
 
@@ -74,21 +133,13 @@ public final class BitVector {
             return this;
         }
 
+        /**
+         * Creates a new BitVector
+         * 
+         * @return the newly created BitVector, not null
+         */
         public BitVector build() {
             checkIfBuiltAlready();
-
-            // int length = bytes.length;
-            // int[] temp = new int[length / (Integer.SIZE / Byte.SIZE)];
-            //
-            // for (int i = 0; i < temp.length; i++) {
-            // int a = 0;
-            // int intByteRatio = Integer.SIZE / Byte.SIZE;
-            // for (int j = 0; j < intByteRatio; j++) {
-            // a += Byte.toUnsignedInt(
-            // bytes[i * intByteRatio + j]) << (Byte.SIZE * j);
-            // }
-            // temp[i] = a;
-            // }
 
             enable = false;
             return new BitVector(bits);
@@ -101,15 +152,35 @@ public final class BitVector {
         }
     }
 
+    /**
+     * gives the number of bits in the vector
+     * 
+     * @return an integer : the number of bits in the vectors
+     */
     public int size() {
         return vector.length * Integer.SIZE;
     }
 
+    /**
+     * gives the value of the bit in the vector at the given index
+     * 
+     * @param index
+     *            : an integer the index of the bit we want to test
+     * @return a boolean : true if the bit is one, false if it is zero
+     * @throws IndexOutOfBoundsException
+     *             : if the index is less than zero or bigger than the size of
+     *             the vector
+     */
     public boolean testBit(int index) {
         Objects.checkIndex(index, this.size());
         return Bits.test(vector[index / Integer.SIZE], index % Integer.SIZE);
     }
 
+    /**
+     * Computes the one's-complement of the BitVector
+     * 
+     * @return a BitVector : the one's-complement of the current BitVector
+     */
     public BitVector not() {
         int[] elements = new int[vector.length];
         int i = 0;
@@ -120,54 +191,107 @@ public final class BitVector {
         return new BitVector(elements);
     }
 
+    /**
+     * Computes the bit-by-bit logical conjunction "and" of the current
+     * BitVector with another BitVector
+     * 
+     * @param that
+     *            : a BitVector, the BitVector with which we want to compute the
+     *            logical conjunction
+     * @return a BitVector : the result of the bit-by-bit conjunction "and" of
+     *         the current BitVector and that
+     */
     public BitVector and(BitVector that) {
         return andOr(that, true);
     }
 
+    /**
+     * Computes the bit-by-bit logical disjunction "or" of the current BitVector
+     * with another BitVector
+     * 
+     * @param that
+     *            : a BitVector, the BitVector with which we want to compute the
+     *            logical disjunction
+     * @return a BitVector : the result of the bit-by-bit disjunction "or" of
+     *         the current BitVector and that
+     */
     public BitVector or(BitVector that) {
         return andOr(that, false);
     }
+    
 
+    //TODO pourquoi on a fait ça?? apparememnt y'a pas besoin
+    public BitVector xor(BitVector that) {
+        Preconditions.checkArgument(that.size() == size());
+        int length = vector.length;
+
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            int other = that.vector[i];
+            int me = vector[i];
+            result[i] = other ^ me;
+        }
+
+        return new BitVector(result);
+    }
+
+    /**
+     * Extracts a BitVector of the given length, from the infinite extension by
+     * zero of the current BitVector, from given index
+     * 
+     * @param index
+     *            : an integer, the index from which we extract the new
+     *            BitVector in the infinite vector
+     * @param length
+     *            : an integer, the length of the BitVector we want to extract
+     * @return a BitVector : new the BitVector extracted from the infinite
+     *         extension by zero of the current BitVector
+     * @throws IllegalArgumentException
+     *             : if the length is less or equal to zero, or if it is not a
+     *             multiple of 32
+     */
     public BitVector extractZeroExtended(int index, int length) {
         return new BitVector(extract(index, length, Extraction.ZERO_EXTENDED));
     }
 
+    /**
+     * Extracts a BitVector of the given length, from the infinite wrapped
+     * extension of the current BiVector, from the given index
+     * 
+     * @param index
+     *            : an integer, the index from which we extract the new
+     *            BitVector in the infinite vector
+     * @param length
+     *            : an integer, the length of the BitVector we want to extract
+     * @return a BitVector : the new BitVector extracted from the infinite
+     *         wrapped extension of the current BitVector
+     * @throws IllegalArgumentException
+     *             : if the length is less or equal to zero, or if it is not a
+     *             multiple of 32
+     */
     public BitVector extractWrapped(int index, int length) {
         return new BitVector(extract(index, length, Extraction.WRAPPED));
     }
 
+    /**
+     * Shifts the BitVector the given distance (to the left if the distance is
+     * positive, right if the distance is negative), by adding zeros at the
+     * opposite extremity
+     * 
+     * @param distance
+     *            : an integer, the distance we want to do the shifting
+     * @return a BitVector : the new BitVector that is the shifted current
+     *         vector
+     */
     public BitVector shift(int distance) {
         return new BitVector(
                 extract(-distance, size(), Extraction.ZERO_EXTENDED));
     }
 
-    private int[] extract(int index, int length, Extraction type) {
-
-        Preconditions.checkArgument(is32Multiple(length));
-
-        int arrayIndex = Math.floorDiv(index, Integer.SIZE);
-        int intIndex = Math.floorMod(index, Integer.SIZE);
-
-        int nbIntegersToCompute = Math.floorDiv(length, Integer.SIZE);
-        int[] array = new int[nbIntegersToCompute];
-
-        int temp1 = computeInt(type, arrayIndex);
-        int temp2 = 0;
-        for (int i = 0; i < nbIntegersToCompute; i++) {
-            if (is32Multiple(index)) {
-                array[i] = computeInt(type, i + arrayIndex);
-            } else {
-                temp2 = computeInt(type, i + arrayIndex + 1);
-                temp1 = temp1 >>> intIndex;
-                array[i] = (temp2 << (32 - intIndex)) | temp1;
-
-                temp1 = temp2;
-            }
-        }
-
-        return array;
-    }
-
+    /**
+     * non java-doc
+     * @see java.lang.Object
+     */
     @Override
     public boolean equals(Object that) {
         Preconditions.checkArgument(that instanceof BitVector);
@@ -182,11 +306,19 @@ public final class BitVector {
         return true;
     }
 
+    /**
+     * non java-doc
+     * @see java.lang.Object
+     */
     @Override
     public int hashCode() {
         return vector.hashCode();
     }
 
+    /**
+     * non java-doc
+     * @see java.lang.Object
+     */
     @Override
     public String toString() {
         String binary = "";
@@ -213,6 +345,7 @@ public final class BitVector {
     }
 
     private BitVector andOr(BitVector that, boolean and) {
+        Objects.requireNonNull(that);
         Preconditions.checkArgument(that.size() == size());
         int length = vector.length;
 
@@ -225,24 +358,35 @@ public final class BitVector {
 
         return new BitVector(result);
     }
+    
+    private int[] extract(int index, int length, Extraction type) {
 
-    public BitVector xor(BitVector that) {
-        Preconditions.checkArgument(that.size() == size());
-        int length = vector.length;
+        Preconditions.checkArgument(is32Multiple(length) && length != 0);
 
-        int[] result = new int[length];
-        for (int i = 0; i < length; i++) {
-            int other = that.vector[i];
-            int me = vector[i];
-            result[i] = other ^ me;
+        int arrayIndex = Math.floorDiv(index, Integer.SIZE);
+        int intIndex = Math.floorMod(index, Integer.SIZE);
+
+        int nbIntegersToCompute = Math.floorDiv(length, Integer.SIZE);
+        int[] array = new int[nbIntegersToCompute];
+
+        int temp1 = computeInt(type, arrayIndex);
+        int temp2 = 0;
+        for (int i = 0; i < nbIntegersToCompute; i++) {
+            if (is32Multiple(index)) {
+                array[i] = computeInt(type, i + arrayIndex);
+            } else {
+                temp2 = computeInt(type, i + arrayIndex + 1);
+                temp1 = temp1 >>> intIndex;
+                array[i] = (temp2 << (32 - intIndex)) | temp1;
+
+                temp1 = temp2;
+            }
         }
 
-        return new BitVector(result);
+        return array;
     }
 
-    // TODO il faudra supprimer ça quand on en n'aura plus besooin pour les
-    // tests
-    public int[] extractP(int index, int length, Extraction type) {
+    private int[] extractP(int index, int length, Extraction type) {
         return extract(index, length, type);
     }
 }
