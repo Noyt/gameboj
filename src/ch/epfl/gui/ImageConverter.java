a9d8b14c5d73ec23d40cae8919da9f28f12d4814
Arnaud Robert <arnaudemmanuel.robert@epfl.ch>
2018-05-09 13:39:07 +0200

    debut javaFX

--- src/ch/epfl/gui/ImageConverter.java
+++ src/ch/epfl/gui/ImageConverter.java
@@ -2,10 +2,26 @@ package ch.epfl.gui;
 
 import ch.epfl.gameboj.component.lcd.LcdImage;
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 
 public final class ImageConverter {
     
+    private static final int[] COLOR_MAP = new int[] {
+            0xFF_FF_FF, 0xD3_D3_D3, 0xA9_A9_A9, 0x00_00_00
+    };
+
     public static Image convert(LcdImage image) {
+        int width = image.width();
+        int height = image.height();
+        WritableImage wi = new WritableImage(width, height);
+        PixelWriter writer = wi.getPixelWriter();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                writer.setArgb(i, j, COLOR_MAP[image.get(i, j)]);
+            }
+        }
         
+        return wi;
     }
 }
--- src/ch/epfl/gui/Main.java
+++ src/ch/epfl/gui/Main.java
@@ -1,19 +1,44 @@
 package ch.epfl.gui;
 
+import java.io.File;
 import java.io.IOException;
 
+import ch.epfl.gameboj.GameBoy;
+import ch.epfl.gameboj.Preconditions;
+import ch.epfl.gameboj.component.cartridge.Cartridge;
 import javafx.application.Application;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public final class Main extends Application {
 
+    private static String ROM_PATH;
+    
     public static void main(String[] args) {
+        Preconditions.checkArgument(args.length == 1);
         Application.launch(args);
+        ROM_PATH = args[0];
     }
     
     @Override
     public void start(Stage stage) throws IOException, InterruptedException {
+        //Create GameBoy
+        if(getParameters().getRaw().size() != 1) {
+            System.exit(1);
+        }
+        File romFile = new File(ROM_PATH);
+        GameBoy gb = new GameBoy(Cartridge.ofFile(romFile));
         
+        //Creates GUI
+        ImageView imageView = new ImageView();
+        Image image = getImage(gb);
+        imageView.setFitWidth(image.getWidth()*2);
+        imageView.setFitHeight(image.getHeight()*2);
+        imageView.setImage(image);
     }
-
+    
+    private static final Image getImage(GameBoy gb) {
+        return ImageConverter.convert(gb.lcdController().currentImage());
+    }
 }
