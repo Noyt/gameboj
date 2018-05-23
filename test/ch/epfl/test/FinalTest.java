03ebb685893c39b286927149496df886a7d54d26
Arnaud Robert <arnaudemmanuel.robert@epfl.ch>
2018-05-23 17:01:28 +0200

    doc lcdController

--- test/ch/epfl/test/FinalTest.java
+++ test/ch/epfl/test/FinalTest.java
@@ -13,11 +13,11 @@ public class FinalTest {
         //Main.main(new String[] {"finalflappyboy.gb"});
         //Main.main(new String[] {"Tetris.gb"});
         //Main.main(new String[] {"Super_Mario_Land.gb"});
-        Main.main(new String[] {"Super_Mario_Land_2.gb"});
+        //Main.main(new String[] {"Super_Mario_Land_2.gb"});
         //Main.main(new String[] {"2048.gb"});
         //Main.main(new String[] {"snake.gb"});
         //Main.main(new String[] {"Donkey_Kong.gb"});
         //Main.main(new String[] {"Bomberman.gb"});
-        //Main.main(new String[] {"Zelda.gb"});
+        Main.main(new String[] {"Zelda.gb"});
     }
 }
